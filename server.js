ae3b2e58080fc9cfbeefbc1d027cecc7d919cbd3
jacobharper259 <jacobharper259@gmail.com>
2022-06-20 23:18:08 -0400

    added another coredrinks url

--- server.js
+++ server.js
@@ -252,7 +252,9 @@ app.get('/roasts',(req,res)=>{
 app.get('/coredrinks',(req,res)=>{
     res.sendFile(__dirname+'/public/coreDrinks.html')
 })
-
+app.get('/coredrinks/',(req,res)=>{
+    res.sendFile(__dirname+'/public/coreDrinks.html')
+})
 
 app.get('/api/coreDrinks'.toLowerCase(),(req,res)=>{
     res.json(coreDrinks)
