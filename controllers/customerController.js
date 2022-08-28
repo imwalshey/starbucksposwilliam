73c57ca181302387feecd1efff9713546726f8b3
jacobharper259 <jacobharper259@gmail.com>
2022-08-27 22:12:25 -0400

    Bug in reading drinksArray 'null' elements when they dont need to be read. Fixed with an IF statement

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -7,7 +7,7 @@ const customerModel = require('../models/customers')
 module.exports={
     sendCustomer :(req,res)=>{
         let randomNum = Math.floor(Math.random() * (customerModel.customer.length))
-        //randomNum = 168
+        //randomNum = 134
         res.json(customerModel.customer[randomNum])
     },
     sendAllCustomers :(req,res)=>{
--- controllers/mainController.js
+++ controllers/mainController.js
@@ -21,33 +21,33 @@ module.exports={
             let checker = (arr, target) => target.every(v => arr.includes(v));
             let points = 0
             req.body.drinksArray.forEach((drink,i)=>{
-                if(req.body.drinksArray[i]!==undefined){
-                let answer
-                let correctAnswer = JSON.parse(JSON.stringify(customerController.customerCorrectAnswers[Number(req.body.customerID)]))
-                if(req.body.drinkIsIced[i]===true){
-                    answer = req.body.drinksArray[i].iced
-                    delete req.body.drinksArray[i].iced.ogPumps
-                    delete req.body.drinksArray[i].iced.ogShots
+                if(req.body.drinksArray[i]!==undefined && req.body.drinksArray[i]!==null){
+                    let answer
+                    let correctAnswer = JSON.parse(JSON.stringify(customerController.customerCorrectAnswers[Number(req.body.customerID)]))
+                    if(req.body.drinkIsIced[i]===true){
+                        answer = req.body.drinksArray[i].iced
+                        delete req.body.drinksArray[i].iced.ogPumps
+                        delete req.body.drinksArray[i].iced.ogShots
+                    }
+                    if(req.body.drinkIsIced[i]===false){
+                        answer = req.body.drinksArray[i].hot
+                        delete req.body.drinksArray[i].hot.ogShots
+                        delete req.body.drinksArray[i].hot.ogPumps
+                    }
+                    console.log( answer.decaf)
+                    console.log( correctAnswer.decaf)
+                    if(checker(answer.decaf,correctAnswer.decaf)){
+                        delete answer.decaf
+                        delete correctAnswer.decaf
+                    }
+                    if(JSON.stringify(answer) ===JSON.stringify(correctAnswer)){
+                        points+=1
+                    }
+                    console.log('answer')
+                    console.log(answer)
+                    console.log('correctAnswer')
+                    console.log(correctAnswer)
                 }
-                if(req.body.drinkIsIced[i]===false){
-                    answer = req.body.drinksArray[i].hot
-                    delete req.body.drinksArray[i].hot.ogShots
-                    delete req.body.drinksArray[i].hot.ogPumps
-                }
-                console.log( answer.decaf)
-                console.log( correctAnswer.decaf)
-                if(checker(answer.decaf,correctAnswer.decaf)){
-                    delete answer.decaf
-                    delete correctAnswer.decaf
-                }
-                if(JSON.stringify(answer) ===JSON.stringify(correctAnswer)){
-                    points+=1
-                }
-                console.log('answer')
-                console.log(answer)
-                console.log('correctAnswer')
-                console.log(correctAnswer)
-            }
             })
             if(points>=1){
                     res.json('win')
