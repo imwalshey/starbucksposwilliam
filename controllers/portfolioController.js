bd8d4926b8ee344cf8b0f399b62cb3c19f8cd7dc
jacobharper259 <jacobharper259@gmail.com>
2022-09-08 15:45:31 -0400

    Defined path for portfolio directory

--- controllers/portfolioController.js
+++ controllers/portfolioController.js
@@ -2,6 +2,7 @@ const express =require('express')
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+let path = (__dirname.split('/controllers')[0]+'/public')
 module.exports={
     index: (req,res)=>{
         res.sendFile(path+'/portfolio/index.html')
