7ed78ec2e3868e06a469cc5e3c17d468036cdc4d
jacobharper259 <jacobharper259@gmail.com>
2022-09-11 22:39:35 -0400

    Finished writing the Partner Hours Page

--- public/PartnerHours/main.js
+++ public/PartnerHours/main.js
@@ -1,4 +1,11 @@
-
+class CalendarDate{
+  constructor(Date,TimeS,TimeE,DayOfTheWeek){
+      this.date = Date
+      this.timeStart = TimeS
+      this.timeEnd = TimeE
+      this.dayOfTheWeek =DayOfTheWeek
+  }
+}
 
 
 
@@ -10,8 +17,8 @@ document.querySelector('.submit').addEventListener('click',()=>{
   let words = document.querySelector('.textarea').innerText
   convertTextToCalendar(words)
 })
-//beforeText = document.querySelector('.textarea').innerText
-
+beforeText = document.querySelector('.textarea').innerText
+let calendarInfoArray = []
 const d = new Date();
 let month = d.getMonth() + 1;
 let year = d.getFullYear();
@@ -24,7 +31,6 @@ document.querySelector('.monthSelect').value = `${year}-${monthAdjusted}`
 function convertTextToCalendar(input){
   
   input = input.replace(/(\r\n|\n|\r)/gm, " ");
-  console.log(input)
   let textStrings =[]
   let beforeText = input
   beforeText = beforeText.replace(/[^a-zA-Z0-9,: ]/g, '')
@@ -37,7 +43,7 @@ function convertTextToCalendar(input){
           indexes.push(i)
       }
   })
-  console.log(indexes)
+  //console.log(indexes)
   indexes.forEach((num,i)=>{
       let indexSize = indexes[i+1] -num
       if(isNaN(indexSize)){
@@ -45,21 +51,23 @@ function convertTextToCalendar(input){
       }
       textStrings.push(beforeText.split(' ').splice(indexes[i],indexSize).join(' '))
   })
-  console.log(textStrings)
+  //console.log(textStrings)
     let datesArray = []
   textStrings.forEach((string,s)=>{
     let date
     let startTime
     let endTime
-
+    let dayOfTheWeek
     let times = []
     let amPm = []
     //console.log(string)
     string.split(' ').forEach((elem,i)=>{
       //console.log(Number(elem))
       if(elem==='Mon' || elem==='Tue' || elem==='Wed'|| elem==='Eri'|| elem==='Thu'|| elem==='Fri'|| elem==='Sat'|| elem==='Sun'){
-        // console.log(elem)
-        // console.log(string.split(' ')[i+2])
+        dayOfTheWeek=elem
+        if(dayOfTheWeek.toLowerCase()==='eri'){
+          dayOfTheWeek='Fri'
+        }
       }
       if(Number(elem)<=31 && !isNaN(Number(elem)) && elem != ''){
         //console.log(elem)
@@ -89,15 +97,80 @@ function convertTextToCalendar(input){
       month = month + 1
     }
     if(!string.toLowerCase().includes('off') && !string.toLowerCase().includes('shift')){
-      console.log(`${month}/${date}/${year} ${startTime}`, `${month}/${date}/${year} ${endTime}`)
-      cal.addEvent('Work', '', 'Work', `${month}/${date}/${year} ${startTime}`, `${month}/${date}/${year} ${endTime}`)
+      //console.log(`${month}/${date}/${year} ${startTime}`, `${month}/${date}/${year} ${endTime}`)
+      //cal.addEvent('Work', '', 'Work', `${month}/${date}/${year} ${startTime}`, `${month}/${date}/${year} ${endTime}`)
+      calendarInfoArray.push(new CalendarDate(`${month}/${date}/${year}`,`${startTime}`,`${endTime}`,`${dayOfTheWeek}`))
     }
     
     //console.log(datesArray[s])
+    
   })
-  //cal.download('Work Schedule');
+  askUserConfirmation()
+  
+  
+  
 }
-//convertTextToCalendar(beforeText)
+let downloadButton = document.querySelector('.download')
+function askUserConfirmation(){
+  downloadButton.innerText=`Download ${calendarInfoArray.length} Calendar Events`
+  document.querySelector('.inputSec').classList.add('hidden')
+  document.querySelector('.confirmations').classList.remove('hidden')
+  document.querySelector('.addButton').classList.remove('hidden')
+  calendarInfoArray.forEach((elem,i)=>{
+    //console.log(elem)
+    let div = document.createElement('div')
+    div.classList.add('weekDay')
+    div.classList.add(`calendarItem${i}`)
+    div.innerHTML = `
+      <div class = dateGrouper>
+          <div class = 'day'>${elem.dayOfTheWeek}</div>
+          <div class = 'date'>${elem.date.split('/')[1]}</div>
+      </div>
+      <div class="timeToWork">${elem.timeStart}-${elem.timeEnd}</div>
+      <div class='addOrRemove cancel'><i class="fa-solid fa-x"></i></div>
+    `
+    document.querySelector('.confirmations').appendChild(div)
+    document.querySelector(`.calendarItem${i} .addOrRemove`).addEventListener('click',()=>{
+      
+      let button = document.querySelector(`.calendarItem${i} .addOrRemove`)
+      if(button.classList.contains('cancel')){
+        button.classList.remove('cancel')
+        button.innerHTML='<i class="fa-solid fa-check">'
+        document.querySelector(`.calendarItem${i}`).classList.add('dull')
+      }else{
+         button.classList.add('cancel') 
+         button.innerHTML='<i class="fa-solid fa-x"></i>'
+         document.querySelector(`.calendarItem${i}`).classList.remove('dull')
+      }
+      downloadButton.innerText=`Download ${calendarInfoArray.length - document.querySelectorAll('.dull').length} Calendar Events`
+    })
+    
+  })
+  document.querySelector('.download').addEventListener('click',()=>{
+    compileCalendar()
+  })
+  
+}
+function compileCalendar(){
+  document.querySelectorAll('.weekDay').forEach((elem)=>{
+    let index = elem.classList[1].split('calendarItem')[1]
+    if(!elem.classList.contains('dull')){
+      console.log(calendarInfoArray[index].timeStart)
+      cal.addEvent('Work', '', 'Work', `${calendarInfoArray[index].date} ${calendarInfoArray[index].timeStart}`, `${calendarInfoArray[index].date} ${calendarInfoArray[index].timeEnd}`)
+    }
+  })
+  cal.download(`${calendarInfoArray[0].date}-${calendarInfoArray[calendarInfoArray.length-1].date}`)
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
