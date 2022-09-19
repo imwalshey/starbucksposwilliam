211aa0ef338ddc96ddab6382ccf2078641046cc6
jacobharper259 <jacobharper259@gmail.com>
2022-09-19 08:56:34 -0400

    fixed a routing error

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -24,7 +24,7 @@ module.exports={
         res.sendFile(path+'/roasts.html')
     },
     menu: (req,res)=>{
-        res.sendFile(path+'/pos/menu.html')
+        res.sendFile(path+'/Menu/menu.html')
     },
     coredrinks: (req,res)=>{
         res.sendFile(path+'/CoreDrinks/coreDrinks.html')
